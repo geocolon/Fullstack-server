c766a46f24a7672f35368951911743ff1b4ba095
George Colon <geocolon@gmail.com>
2018-06-11 09:27:09 -0700

    fixed endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
 const Note = require('../models/note');
 const User = require('../models/user');
@@ -17,14 +19,10 @@ const User = require('../models/user');
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 router.get('/', (req, res) => {
-  
   Note
     .find()
     .then(notes => {
-      res.json({
-        notes: notes.map(
-          note => note.serialize())
-      });
+      res.json(notes);
     })
     .catch(
       err => {
@@ -32,14 +30,19 @@ router.get('/', (req, res) => {
         res.status(500).json({message:'Inernal server error'});
       });
 });
-
 // Find by ID
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
+  console.log('Values of the req. ',req);
   Note
-    .then(console.log(req.params))
     .findById(req.params.id)
-    .then(note => res.json(note.serialize()))
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Inernal server error'});
@@ -50,7 +53,7 @@ router.get('/:id', (req, res) => {
 // got required fields ('name' and 'checked'). if not,
 // log an error and return a 400 status code. if okay,
 // add new item to ShoppingList and return it with a 201.
-router.post('/', jsonParser, (req, res) => {
+router.post('/', [jwtAuth, jsonParser], (req, res, next) => {
   // ensure `name` and `text` are in request body
   const requiredFields = ['name', 'text'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -63,28 +66,37 @@ router.post('/', jsonParser, (req, res) => {
   }
   return Note.create({name:req.body.name, text:req.body.text})
     .then(data => {
-      return User.findByIdAndUpdate(req.username, {$push: {notes: data._id}})
-        .then(() => data);
+      console.log(req.user.username);
+      return User.findOne({username: req.user.username})
+        .then((user) => {
+          user.notes.push(data.id);
+          return user.save();
+        });
     })
-    .then(item => res.status(201).json(item));
+    .then(item => res.status(201).json(item))
+    .catch(err => {
+      next(err);
+    });
 });
+  
 
 
 // when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-router.delete('/:id', (req, res) => {
+// try to delete that item from Note.
+router.delete('/:id', [jwtAuth, jsonParser], (req, res) => {
   Note
     .findByIdAndRemove(req.params.id)
     .then(() => res.status(204).end())
-    .catch(err => res.status(500).json({message:'internal server erro'}));
+    .catch(err => res.status(500)
+      .json(err, {message:'internal server error'}));
 });
 
 // when PUT request comes in with updated item, ensure has
 // required fields. also ensure that item id in url path, and
 // item id in updated item object match. if problems with any
 // of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-router.put('/:id', jsonParser, (req, res) => {
+// call `Note.update` with updated item.
+router.put('/:id', [jwtAuth, jsonParser], (req, res) => {
   const requiredFields = ['name', 'text', 'id'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -101,7 +113,7 @@ router.put('/:id', jsonParser, (req, res) => {
     console.error(message);
     return res.status(400).send(message);
   }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
+  console.log(`Updating note list item \`${req.params.id}\``);
   const updatedItem = ShoppingList.update({
     id: req.params.id,
     name: req.body.name,
